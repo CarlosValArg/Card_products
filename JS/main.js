8c5119c16f435af9ef10c4ab040bb08d0c42c65e
Carlos Valencia <cavalar1999@gmail.com>
2024-09-06 00:29:46 -0600

    Se agrega el ejercicio de Cards terminado

--- JS/main.js
+++ JS/main.js
@@ -16,21 +16,23 @@ function getData(){
         products.forEach(p =>{
             divData.insertAdjacentHTML("beforeend",
                 `<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="${p.image}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">${p.name}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">${p.brand}</li>
+    <li class="list-group-item">${p.description}</li>
+    <li class="list-group-item">${p.rating}</li>
   </ul>
   <div class="card-body">
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div>
 </div>`
-            )
-        })
-    }
+            );
+        });
+    }/**createCards */
+
+    getData();
